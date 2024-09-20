7ec71482d28194be052820b15671ccb8324b775e
Devendra Kumar Rathore <devendra.rathore@gyansys.com>
2024-09-20 13:29:03 +0530

    testing the submodule

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import  Home from './submodule-a/src/home';
-const Home = React.lazy(() => import('./submodule-a/src/home'));
+import  Home from './submodule-a/src/Pages/home';
+// const Home = React.lazy(() => import('./submodule-a/src/home'));
 
 function App() {
   return (
